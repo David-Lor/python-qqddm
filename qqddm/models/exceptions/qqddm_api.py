7b044db8f8136f8a3f75d0177eea32c0df4f439c
David Lorenzo <17401854+David-Lor@users.noreply.github.com>
2022-12-08 00:04:39 +0100

    fix: QQ API changes since 2022-12-06 (#6)
    * docs: document more failure response bodies
    
    * feat: add new required x-sign headers; support more API errors with custom exceptions
    
    * docs: document required request headers
    
    * docs(changelog): v0.0.2
    
    * docs(README): document requirement of requesting API from China
    
    * chore: add proxy env to example script

--- qqddm/models/exceptions/qqddm_api.py
+++ qqddm/models/exceptions/qqddm_api.py
@@ -3,7 +3,10 @@ from typing import Optional
 from .base import BaseQQDDMException
 from ..qqddm_api import AIProcessorResponseBody
 
-__all__ = ["BaseQQDDMApiException", "InvalidQQDDMApiResponseException", "IllegalPictureQQDDMApiResponseException"]
+__all__ = [
+    "BaseQQDDMApiException", "InvalidQQDDMApiResponseException", "IllegalPictureQQDDMApiResponseException",
+    "RESPONSE_CODES_EXCEPTIONS"
+]
 
 
 class BaseQQDDMApiException(BaseQQDDMException):
@@ -29,3 +32,28 @@ class InvalidQQDDMApiResponseException(BaseQQDDMApiException):
 
 class IllegalPictureQQDDMApiResponseException(InvalidQQDDMApiResponseException):
     error_msg = "Illegal picture"
+
+
+class VolumnLimitQQDDMApiResponseException(InvalidQQDDMApiResponseException):
+    error_msg = "API rate limited or picture too big"
+
+
+class AuthFailedQQDDMApiResponseException(InvalidQQDDMApiResponseException):
+    error_msg = "Auth failed (the API may have changed and the library is currently not compatible)"
+
+
+class NotAllowedCountryQQDDMApiResponseException(InvalidQQDDMApiResponseException):
+    error_msg = "The current country is blocked by the API"
+
+
+class NoFaceInPictureQQDDMApiResponseException(InvalidQQDDMApiResponseException):
+    error_msg = "The picture does not have a valid face"
+
+
+RESPONSE_CODES_EXCEPTIONS = {
+    2114: IllegalPictureQQDDMApiResponseException,
+    2111: VolumnLimitQQDDMApiResponseException,
+    -2111: AuthFailedQQDDMApiResponseException,
+    2119: NotAllowedCountryQQDDMApiResponseException,
+    1001: NoFaceInPictureQQDDMApiResponseException,
+}
