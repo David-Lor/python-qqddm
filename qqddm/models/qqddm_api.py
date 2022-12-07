5fb176919770ad6d57066b334e3a2e41104ad2f9
David-Lor <17401854+David-Lor@users.noreply.github.com>
2022-12-07 22:18:19 +0100

    feat: add new required x-sign headers; support more API errors with custom exceptions

--- qqddm/models/qqddm_api.py
+++ qqddm/models/qqddm_api.py
@@ -2,12 +2,22 @@ from typing import List, Optional
 
 import pydantic
 
+from ..utils import get_uuid4
+
 
 class AIProcessorRequestBody(pydantic.BaseModel):
 
     class Extra(pydantic.BaseModel):
-        version: Optional[int] = None
+
+        class DataReport(pydantic.BaseModel):
+            parent_trace_id: str = pydantic.Field(default_factory=get_uuid4)
+            root_channel: str = ""
+            level: int = 0
+
+        version: int = 2
         platform: str = "web"
+        face_rects: list = []
+        data_report: DataReport = pydantic.Field(default_factory=DataReport)
 
     busiId: str = "ai_painting_anime_entry"
     images: List[str]
